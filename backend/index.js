3e675f97701cec6975e6ab718d159ce59276d954
Nitin16May <108407872+Nitin16May@users.noreply.github.com>
2024-03-20 00:47:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,19 @@ const app = express()
 const port = 5000
 const mongoDb = require("./db")
 
-// app.use((req, res, next)=>{
-//   res.setHeader('Access-Control-Allow-Origin', process.env.REACT_APP_FRONTEND_URL);
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// })
+app.use((req, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+})
 
 mongoDb();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('API Endpoint for CR-TG')
 })
 
 
