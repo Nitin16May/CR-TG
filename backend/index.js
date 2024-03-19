7ed5a7f8329472ae89e654b56fc6a5fa5c65ccc5
Nitin16May <108407872+Nitin16May@users.noreply.github.com>
2024-03-20 01:14:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDb = require("./db")
+const cors = require('cors')
 
-app.use((req, res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin', process.env.REACT_APP_FRONTEND_URL);
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-})
+app.use(cors());
 
 mongoDb();
 
